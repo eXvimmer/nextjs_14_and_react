9c7fd560b6d6aabca20fe63c80b30f85a167189c
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2024-05-20 22:51:48 +0330

    02_foodies_project: handle errors in meals page.

---
+++ 02_foodies_project/app/meals/error.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+export default function Error() {
+  return (
+    <main className="error">
+      <h1>Something Went Wrong!</h1>
+      <p>Failed to fetch meal data.</p>
+    </main>
+  );
+}
