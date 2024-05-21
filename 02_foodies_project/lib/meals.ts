9c6d289635cafcf1fdd69d3a6ba1f3d8c45a0375
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2024-05-21 13:46:20 +0330

    02_foodies_project: handle not-found state.

--- 02_foodies_project/lib/meals.ts
+++ 02_foodies_project/lib/meals.ts
@@ -5,6 +5,5 @@ const db = sql("meals.db");
 
 export async function getMeals(): Promise<Meal[]> {
   // TODO: remove the timeout
-  await new Promise((resolve) => setTimeout(resolve, 2000));
   return db.prepare(`SELECT * FROM meals`).all() as Meal[];
 }
