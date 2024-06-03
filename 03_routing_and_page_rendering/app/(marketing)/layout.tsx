024a27a94effbfe4d8169a5c3d68dc9903c69606
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2024-06-03 15:38:08 +0330

    03_routing_and_page_rendering: use route groups to create different layouts for the main page and others.

--- 03_routing_and_page_rendering/app/(marketing)/layout.tsx
+++ 03_routing_and_page_rendering/app/(marketing)/layout.tsx
@@ -1,5 +1,4 @@
-import MainHeader from "@/components/MainHeader";
-import "./globals.css";
+import "../globals.css";
 
 export const metadata = {
   title: "Next.js Page Routing & Rendering",
@@ -13,12 +12,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>
-        <div id="page">
-          <MainHeader />
-          {children}
-        </div>
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
