87932fadddac57feb7aded1121015a91d305078c
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2024-06-03 15:25:41 +0330

    03_routing_and_page_rendering: navigate programmatically from the intercepted parallel route the previous page.

--- 03_routing_and_page_rendering/app/news/[slug]/@modal/(.)image/page.tsx
+++ 03_routing_and_page_rendering/app/news/[slug]/@modal/(.)image/page.tsx
@@ -1,18 +1,21 @@
+"use client";
+
 import { DUMMY_NEWS } from "@/dummy_news";
-import { notFound } from "next/navigation";
+import { notFound, useRouter } from "next/navigation";
 
 export default function ImagePage({
   params: { slug },
 }: {
   params: { slug: string };
 }) {
+  const router = useRouter();
   const n = DUMMY_NEWS.find((n) => n.slug === slug);
   if (!n) {
     notFound();
   }
   return (
     <>
-      <div className="modal-backdrop" />
+      <div className="modal-backdrop" onClick={router.back} />
       <dialog className="modal" open>
         <div className="fullscreen-image">
           <img src={`/images/news/${n.image}`} alt={n.title} />
