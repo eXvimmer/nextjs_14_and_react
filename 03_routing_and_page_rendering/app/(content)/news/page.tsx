eeac64f713862d959e737dc6c38a379300bca92c
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2024-06-13 11:49:01 +0330

    03_routing_and_page_rendering: use server-side data fetching techniques to improve SEO.

--- 03_routing_and_page_rendering/app/(content)/news/page.tsx
+++ 03_routing_and_page_rendering/app/(content)/news/page.tsx
@@ -1,39 +1,17 @@
-"use client";
-
 import { INews } from "@/dummy_news";
 import NewsList from "@/components/NewsList";
-import { useEffect, useState } from "react";
 
-export default function NewsPage() {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState("");
-  const [news, setNews] = useState<INews[]>([]);
-
-  useEffect(() => {
-    async function fetchNews() {
-      setIsLoading(true);
-      const res = await fetch("http://localhost:8080/news");
-      if (!res.ok) {
-        setError("Failed to fetch news.");
-      }
-      setIsLoading(false);
-      const fetchedNews: INews[] = await res.json();
-      setNews(fetchedNews);
-    }
-
-    fetchNews();
-  }, []);
+export default async function NewsPage() {
+  const res = await fetch("http://localhost:8080/news");
+  if (!res.ok) {
+    throw new Error("Failed to fetch news.");
+  }
+  const news: INews[] = await res.json();
 
   return (
     <>
       <h1>News Page</h1>
-      {isLoading ? (
-        <p>Loading...</p>
-      ) : error ? (
-        <p>{error}</p>
-      ) : news ? (
-        <NewsList news={news} />
-      ) : null}
+      <NewsList news={news} />
     </>
   );
 }
