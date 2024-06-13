db37e97af194c914124aadadf55f7f7542faaeeb
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2024-06-13 11:39:49 +0330

    03_routing_and_page_rendering: use client side data fetching to get news from database.

--- 03_routing_and_page_rendering/app/(content)/news/page.tsx
+++ 03_routing_and_page_rendering/app/(content)/news/page.tsx
@@ -1,11 +1,39 @@
-import { DUMMY_NEWS } from "@/dummy_news";
+"use client";
+
+import { INews } from "@/dummy_news";
 import NewsList from "@/components/NewsList";
+import { useEffect, useState } from "react";
 
 export default function NewsPage() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState("");
+  const [news, setNews] = useState<INews[]>([]);
+
+  useEffect(() => {
+    async function fetchNews() {
+      setIsLoading(true);
+      const res = await fetch("http://localhost:8080/news");
+      if (!res.ok) {
+        setError("Failed to fetch news.");
+      }
+      setIsLoading(false);
+      const fetchedNews: INews[] = await res.json();
+      setNews(fetchedNews);
+    }
+
+    fetchNews();
+  }, []);
+
   return (
     <>
       <h1>News Page</h1>
-      <NewsList news={DUMMY_NEWS} />
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : error ? (
+        <p>{error}</p>
+      ) : news ? (
+        <NewsList news={news} />
+      ) : null}
     </>
   );
 }
