aa3566cd65075903da017f0920ce36d7a5ac3f82
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2024-06-17 15:07:26 +0330

    04_mutating_data: generate metadata dynamically.

--- 04_mutating_data/app/feed/page.tsx
+++ 04_mutating_data/app/feed/page.tsx
@@ -1,5 +1,14 @@
 import Posts from "@/components/posts";
 import { getPosts } from "@/lib/posts";
+import { Metadata } from "next";
+
+export async function generateMetadata(): Promise<Metadata> {
+  const posts = await getPosts();
+  return {
+    title: `Feed Page`,
+    description: `See all ${posts.length} of our posts`,
+  };
+}
 
 export default async function FeedPage() {
   const posts = await getPosts();
