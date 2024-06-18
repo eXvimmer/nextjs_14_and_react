02ace5a327c9c9f4abf6d7195eb13d12476a8634
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2024-06-18 17:07:07 +0330

    06_authentication: add an auth-only layout.

---
+++ 06_authentication/app/(auth)/layout.tsx
@@ -0,0 +1,24 @@
+import "../globals.css";
+
+export const metadata = {
+  title: "Next Auth",
+  description: "Next.js Authentication",
+};
+
+export default function AuthRootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <>
+      <header id="auth-header">
+        <p>Welcome back!</p>
+        <form>
+          <button>Logout</button>
+        </form>
+      </header>
+      {children}
+    </>
+  );
+}
