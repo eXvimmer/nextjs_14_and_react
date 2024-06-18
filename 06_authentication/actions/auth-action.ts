e7f688b11fdf7ed4bf77c811b15349e330d3557f
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2024-06-18 11:35:40 +0330

    06_authentication: validate the email and password (weakly), and create a user.

---
+++ 06_authentication/actions/auth-action.ts
@@ -0,0 +1,38 @@
+"use server";
+
+import { hashUserPassword } from "@/lib/hash";
+import { createUser } from "@/lib/user";
+import { IErrors } from "@/types/types";
+import { redirect } from "next/navigation";
+
+export async function signup(_prevState: IErrors, formData: FormData) {
+  const email = formData.get("email") as string;
+  const password = formData.get("password") as string;
+  const errors: IErrors["errors"] = {};
+  // TODO: add real validation
+  if (!email.includes("@")) {
+    errors.email = "Please provide a valid email address.";
+  }
+  if (password.trim().length < 8) {
+    errors.password = "Password should be at least 8 characters long";
+  }
+  if (Object.keys(errors).length) {
+    return {
+      errors,
+    };
+  }
+  const hashedPassword = hashUserPassword(password);
+  try {
+    createUser(email, hashedPassword);
+  } catch (e) {
+    if (e.code === "SQLITE_CONSTRAINT_UNIQUE") {
+      return {
+        errors: {
+          email: "The email has already been taken",
+        },
+      };
+    }
+    throw e;
+  }
+  redirect("/training");
+}
