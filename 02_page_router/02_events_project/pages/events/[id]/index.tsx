5a4608ca55ece1d46e434e16d0450fb11b524571
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2024-06-21 10:51:02 +0330

    02_page_router: 02_events_project: setup the project, convert it to typescript and create the main pages.

---
+++ 02_page_router/02_events_project/pages/events/[id]/index.tsx
@@ -0,0 +1,8 @@
+export default function EventDetailPage() {
+  return (
+    <div>
+      <h1>Selected Event Page</h1>
+      <p>Show selected event</p>
+    </div>
+  );
+}
---
+++ 02_page_router/02_events_project/pages/index.tsx
@@ -0,0 +1,8 @@
+export default function HomePage() {
+  return (
+    <div>
+      <h1>Starting Page</h1>
+      <p>Show featured events</p>
+    </div>
+  );
+}
