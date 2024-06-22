4779df9454f79907aff3e78fc880d739d7997eee
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2024-06-22 19:54:23 +0330

    02_page_router: 03_pre_rendering_and_data_fetching: combine pre-fetching with client-side fetching.

--- 02_page_router/03_pre_rendering_and_data_fetching/pages/last-sales.tsx
+++ 02_page_router/03_pre_rendering_and_data_fetching/pages/last-sales.tsx
@@ -1,9 +1,12 @@
 import { useEffect, useState } from "react";
 import { ISalesItem } from "../data/types";
 import useSWR from "swr";
+import { GetStaticProps, InferGetStaticPropsType } from "next";
 
-export default function LastSalesPage() {
-  const [sales, setSales] = useState<ISalesItem[]>([]);
+export default function LastSalesPage(
+  props: InferGetStaticPropsType<typeof getStaticProps>,
+) {
+  const [sales, setSales] = useState<ISalesItem[]>(props.sales);
   const { data, error } = useSWR(
     `https://nextjs-course-9288e-default-rtdb.asia-southeast1.firebasedatabase.app/sales.json`,
     (url) => fetch(url).then((res) => res.json()),
@@ -26,7 +29,7 @@ export default function LastSalesPage() {
   if (error) {
     return <p>Failed to load.</p>;
   }
-  if (!data || !sales.length) {
+  if (!data && !sales.length) {
     return <p>Loading...</p>;
   }
   return (
@@ -39,3 +42,25 @@ export default function LastSalesPage() {
     </ul>
   );
 }
+
+export const getStaticProps = async function () {
+  try {
+    const res = await fetch(
+      `https://nextjs-course-9288e-default-rtdb.asia-southeast1.firebasedatabase.app/sales.json`,
+    );
+    const data = await res.json();
+    const sales: ISalesItem[] = [];
+    for (const key in data) {
+      sales.push({
+        id: key,
+        username: data[key].username,
+        volume: data[key].volume,
+      });
+    }
+    return { props: { sales } };
+  } catch {
+    return { props: { sales: [] } };
+  }
+} satisfies GetStaticProps<{
+  sales: ISalesItem[];
+}>;
