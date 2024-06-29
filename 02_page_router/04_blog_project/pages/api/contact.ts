e425c58329ad88edf1e25e4652b03f0a653d4e69
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2024-06-29 12:46:49 +0330

    02_page_router: 04_blog_project: store messages with mongodb.

--- 02_page_router/04_blog_project/pages/api/contact.ts
+++ 02_page_router/04_blog_project/pages/api/contact.ts
@@ -1,6 +1,19 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import { MongoClient } from "mongodb";
 
-export default function (req: NextApiRequest, res: NextApiResponse) {
+// TODO: use env variables to change database in production
+const url = "mongodb://localhost:27017";
+const client = new MongoClient(url);
+const dbName = "next_blog";
+
+async function db(collectionName: string) {
+  await client.connect();
+  console.log("Connected successfully to server");
+  const db = client.db(dbName);
+  return db.collection(collectionName);
+}
+
+export default async function (req: NextApiRequest, res: NextApiResponse) {
   if (req.method === "POST") {
     const { email, name, message } = req.body;
     if (
@@ -16,10 +29,18 @@ export default function (req: NextApiRequest, res: NextApiResponse) {
       });
       return;
     }
-    const newMessage = { name, email, message };
-    console.log(newMessage);
-    res
-      .status(201)
-      .json({ message: "Successfully stored the message", newMessage });
+    const newMessage: Record<string, string> = { name, email, message };
+    const messagesCollection = await db("messages");
+    try {
+      const result = await messagesCollection.insertOne(newMessage);
+      newMessage.id = result.insertedId.toString();
+      res
+        .status(201)
+        .json({ message: "Successfully stored the message", newMessage });
+    } catch (e) {
+      console.error(e);
+      res.status(500).json({ message: "Failed to store the message" });
+      return;
+    }
   }
 }
